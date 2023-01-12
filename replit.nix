467f6446bb331d93434c4e052803e8045996c925
ljf7 <>
2023-01-12 09:10:36 +0000

    Update README.md

--- replit.nix
+++ replit.nix
@@ -1,5 +1,20 @@
 { pkgs }: {
   deps = [
+    pkgs.sysstat
+    pkgs.rsync
+    pkgs.tcpdump
+    pkgs.iproute2
+    pkgs.busybox
+    pkgs.mailutils
+    pkgs.mcron
+    pkgs.vim
+    pkgs.wget
+    pkgs.systemd
+    pkgs.toybox
+    pkgs.util-linux.bin
+    pkgs.texinfoInteractive
+    pkgs.man_db
+    pkgs.openssh_with_kerberos
     pkgs.python39Packages.pip
     pkgs.python310
     pkgs.lua
